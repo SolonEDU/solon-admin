81bb47775294e814dbb3a48ee49d55e34de914aa
Kazi Jamal <jamalkazi02@gmail.com>
2020-02-08 13:20:30 -0500

    removed extra code

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.use('/events', eventRoutes);
 app.use('/forum', forumRoutes);
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- routes/votes.js
+++ routes/votes.js
@@ -6,49 +6,6 @@ const axios = require('axios');
 axios.defaults.baseURL = 'https://api.solonedu.com';
 axios.defaults.headers.common['Authorization'] = process.env.AUTHORIZATION;
 
-// Delete Proposal Handle
-router.get('/delete/:proposalID', ensureAuthenticated, (req, res) => {
-	axios
-		.delete(`/proposals/${req.params.proposalID}`)
-		.then(function(response) {
-			req.flash('success_msg', response['data']['message']);
-			res.redirect('/proposals');
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
-});
-
-function addDays(date, days) {
-	var result = new Date(date);
-	result.setDate(result.getDate() + days);
-	return result;
-}
-
-// New Proposal Handle
-router.post('/new', ensureAuthenticated, (req, res) => {
-	const title = req.body.title;
-	const description = req.body.description;
-	const daysremaining = req.body.daysremaining;
-	const starttime = new Date();
-	const endtime = addDays(starttime, parseInt(daysremaining));
-	axios
-		.post('/proposals', {
-			title: title,
-			description: description,
-			starttime: starttime,
-			endtime: endtime,
-			uid: null
-		})
-		.then(function(response) {
-			req.flash('success_msg', response['data']['message']);
-			res.redirect('/proposals');
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
-});
-
 // Get Vote Data Handle
 router.get('/votes/:proposalID', ensureAuthenticated, (req, res) => {
 	const proposalID = req.params.proposalID;
